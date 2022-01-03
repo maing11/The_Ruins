bc7c4fb500e27ba8875d5bb467f2e6e5600e9c80
mai nguyen <maing528@hotmail.com>
2022-01-03 14:45:57 -0800

    First commit

---
+++ The_Ruins/GameView.swift
@@ -0,0 +1,26 @@
+//
+//  GameView.swift
+//  The_Ruins
+//
+//  Created by mai nguyen on 1/3/22.
+//
+
+import SceneKit
+import SpriteKit
+
+
+class GameView: SCNView {
+    override class func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    deinit {
+        
+    }
+    
+    //MARK:- internal functions
+    //MARK:- D-Pad
+    //MARK:- attackButton
+}
