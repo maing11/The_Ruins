885248a810132a75f090b9248cfc749b06b93e85
mai nguyen <maing528@hotmail.com>
2022-01-03 17:05:52 -0800

    Setup SpriteKt Overlay

--- The_Ruins/GameView.swift
+++ The_Ruins/GameView.swift
@@ -10,16 +10,40 @@ import SpriteKit
 
 
 class GameView: SCNView {
-    override class func awakeFromNib() {
+    private var skScene:SKScene!
+    private let overlayNode = SKNode()
+
+    override  func awakeFromNib() {
         super.awakeFromNib()
+        setup2DOverlay()
+
     }
     override func layoutSubviews() {
         super.layoutSubviews()
+        layout2DOverlay()
     }
     deinit {
         
     }
     
+    //MARK:- overlay functions
+    private func setup2DOverlay(){
+        let w = bounds.size.width
+        let h = bounds.size.height
+
+        skScene = SKScene(size: CGSize(width: w, height: h))
+        skScene.scaleMode = .resizeFill
+
+        skScene.addChild(overlayNode)
+        overlayNode.position = CGPoint(x: 0.0, y: h)
+
+        overlaySKScene = skScene
+        skScene.isUserInteractionEnabled = false
+    }
+
+    private func layout2DOverlay() {
+        overlayNode.position = CGPoint(x: 0.0, y: bounds.size.height)
+    }
     //MARK:- internal functions
     //MARK:- D-Pad
     //MARK:- attackButton
