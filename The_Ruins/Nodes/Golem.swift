47599d73bd64ca9103fbc2bd9920c699050e1d2a
mai nguyen <maing528@hotmail.com>
2022-01-08 19:38:45 -0800

    Golem Altitude

--- The_Ruins/Nodes/Golem.swift
+++ The_Ruins/Nodes/Golem.swift
@@ -153,6 +153,28 @@ class Golem: SCNNode {
         } else {
             isWalking = false
         }
+        
+        //update the altidute
+        let initialPosition = position
+        
+        var pos = position
+        var endpoint0 = pos
+        var endpoint1 = pos
+        
+        endpoint0.y -= 0.1
+        endpoint1.y += 0.08
+        
+        let results = scene.physicsWorld.rayTestWithSegment(from: endpoint1, to: endpoint0, options: [.collisionBitMask: BitmaskWall,.searchMode: SCNPhysicsWorld.TestSearchMode.closest])
+        
+        if let result = results.first {
+            let groundAltitude = result.worldCoordinates.y
+            pos.y = groundAltitude
+            
+            position = pos
+        } else {
+            position = initialPosition
+        }
+        
     }
 }
 
